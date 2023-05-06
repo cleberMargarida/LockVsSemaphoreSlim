220cfde545c7bf5f183587f67706d46d8d2d0a8c
cleberMargarida <cleberfilho3@gmail.com>
2023-05-05 23:47:32 -0300

    Add project files.

---
+++ LockVsSemaphoreSlim/Program.cs
@@ -0,0 +1,13 @@
+﻿using BMark;
+
+const int count = 2054;
+object _lock = new object();
+SemaphoreSlim slimSemaphore = new SemaphoreSlim(count, count);
+
+ulong amountToRun = (ulong)(count - PerformanceTester.PreRunAmount - 2);
+
+PerformanceTester.Run("lock", amountToRun, () => { lock (_lock) { } });
+PerformanceTester.Run("SemaphoreSlim.WaitOne", amountToRun, () => { slimSemaphore.Wait(); });
+PerformanceTester.Run("SemaphoreSlim.Release", amountToRun, () => { slimSemaphore.Release(); });
+
+Console.WriteLine(PerformanceTester.GetResults());
